bfff92f61c3075559941658d9c2816351fd45514
Marilzon <maxmaril@hotmail.com>
2018-08-05 12:38:55 -0300

    Refatorando codigo alguns updates visuais

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var users = []
 var userList = document.getElementById('users')
 
-document.getElementById('register').addEventListener('submit', (e) => {
+document.getElementById('signUp').addEventListener('submit', (e) => {
     let name = document.getElementById("name").value
     let email = document.getElementById("email").value
     createUser(name, email)
@@ -14,7 +14,7 @@ function createUser(name, email) {
     }
     users.push(user)
     readUser()
-    document.getElementById('register').reset()
+    document.getElementById('signUp').reset()
 }
 function readUser() {
     userList.innerHTML = ''
@@ -24,7 +24,7 @@ function readUser() {
                 <div class="row">
                     <div class="col s12">
                         <div class="card blue-grey darken-1">
-                            <div class="card-content white-text">
+                            <div class="card-content ">
                                 <p>Nome: ${users[i].name}</p>
                                 <p>E-mail: <u>${users[i].email}</u></p>
                             </div>
@@ -44,13 +44,22 @@ function editUser(index) {
     for (i = 0; i < users.length; i++) {
         if (i == index) {
             userList.innerHTML +=
-        `<li class="user">
+        `
+        <li class="user ">
             <div class="row">
                 <div class="col s12">
                     <div class="card blue-grey darken-1">
-                        <div class="card-content white-text">
-                            <p>Nome: <input class="input-field" type="text" id="updateName"></p>
-                            <p>E-mail: <input class="input-field" type="email" id="updateEmail"></p>
+                        <div class="card-content">
+                        <div class="row">
+                            <div class="input-field col s12 m6">
+                                <label for="updateName">Nome:</label>
+                                <input  type="text" id="updateName">
+                            </div>
+                            <div class="input-field col s12 m6">
+                                <label for="updateEmail">E-mail:</label>
+                                <input type="email" id="updateEmail">
+                            </div>
+                            </div>
                         </div>
                         <div class="card-action">
                             <button class="btn light-blue lighten-2 " onClick="updateUser('${i}')">Atualizar</button>
@@ -63,11 +72,12 @@ function editUser(index) {
         `
         } else {
             userList.innerHTML +=
-            `<li class="user">
+            `
+            <li class="user">
                 <div class="row">
                     <div class="col s12">
                         <div class="card blue-grey darken-1">
-                            <div class="card-content white-text">
+                            <div class="card-content ">
                                 <p>Nome: ${users[i].name}</p>
                                 <p>E-mail: <u>${users[i].email}</u></p>
                             </div>
