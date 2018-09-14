be6409a6f72cd80666d4509557821ad119a27ba5
Marilzon <maxmaril@hotmail.com>
2018-09-14 05:34:27 -0300

    Testes de validãção a Emails repetidos

--- js/main.js
+++ js/main.js
@@ -5,20 +5,31 @@ var nameHelper = document.querySelector("#nameHelper")
 var emailHelper = document.querySelector("#emailHelper")
 var isName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
 var isEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var userList = document.querySelector(".users-list")
 
 userName.addEventListener("input", validateName)
 userEmail.addEventListener("input", validateEmail)
 
-form.setAttribute('novalidate', true)
+form.setAttribute("novalidate", true)
 form.addEventListener("submit", validateName)
 form.addEventListener("submit", validateEmail)
 form.addEventListener("submit", signUp)
 
+function isSigned(user) {
+    if (userEmail.value === user.email) { 
+        submit.classList.add("disabled")
+        emailHelper.classList.add("hidden")
+        nameHelper.classList.add("hidden")
+        M.toast({html: 'Este E-Mail Já esta sendo usado!', classes: 'red'})
+     } else {
+        submit.classList.remove("disabled")
+        emailHelper.classList.remove("hidden")
+        nameHelper.classList.remove("hidden")
+     }
+}
 function validateName() {
     if (userName.value.length < 2 || userName.value.length > 24) {
         nameHelper.classList.remove("green")
-        nameHelper.classList.add("red", "lighten-1")
+        nameHelper.classList.add("red")
         return nameHelper.innerHTML = `Minimo: 2, Maximo: 24 Caractéres`
     }
     else if (
@@ -30,33 +41,34 @@ function validateName() {
     }
     else {
         nameHelper.classList.remove("green")
-        nameHelper.classList.add("red", "lighten-1")
+        nameHelper.classList.add("red")
         return nameHelper.innerHTML = `Nome invalido!`
     }
 }
 function validateEmail() {
     if (userEmail.value === '') {
         emailHelper.classList.remove("green")
-        emailHelper.classList.add("red", "lighten-1")
+        emailHelper.classList.add("red")
         return emailHelper.innerHTML = `Campo obrigatório!`
     }
     else if (
         userEmail.value.match(isEmail)
     ) {
+        users.find(isSigned)
         emailHelper.classList.remove("red")
         emailHelper.classList.add("green")
         return emailHelper.innerHTML = `E-Mail valido!`
     }
     else {
         emailHelper.classList.remove("green")
-        emailHelper.classList.add("red", "lighten-1")
+        emailHelper.classList.add("red")
         return emailHelper.innerHTML = `E-Mail invalido!`
     }
 }
-
 function signUp(e) {
     e.preventDefault()
-    if (userName.value.match(isName) && userEmail.value.match(isEmail)) {
+    if (userName.value.match(isName) &&
+        userEmail.value.match(isEmail)) {
         let name = userName.value
         let email = userEmail.value
         createUser(name, email)
@@ -67,6 +79,7 @@ function createUser(name, email) {
         name: name,
         email: email,
     }
+    M.toast({html: 'Usuario cadastrado!', classes: 'green'})
     users.push(user)
     readUser()
     countUsers()
@@ -85,7 +98,7 @@ function readUser() {
                         <p>E-mail: <u>${users[i].email}</u></p>
                     </div>
                     <div class="card-action">
-                        <button class="btn-small light-blue lighten-2" id="editUser" onclick="editUser('${i}')">Editar</button>
+                        <button class="btn-small light-blue" id="editUser" onclick="editUser('${i}')">Editar</button>
                         <button class="btn-small red darken-2" onclick="removeUser('${i}')">Apagar</button>
                     </div>
                 </div>
@@ -119,7 +132,7 @@ function editUser(index) {
                         </div>
                     </div>
                     <div class="card-action">
-                        <button class="btn light-blue lighten-2" id="updateButton" onClick="updateUser('${i}')">Atualizar</button>
+                        <button class="btn light-blue" id="updateButton" onClick="updateUser('${i}')">Atualizar</button>
                         <button class="btn red darken-2" onClick="readUser()">Cancelar</button>
                     </div>
                 </div>
@@ -130,12 +143,12 @@ function editUser(index) {
                 `
             <li class="user col s12 m6">
                 <div class="card blue-grey darken-1">
-                    <div class="card-content ">
+                    <div class="card-content">
                         <p>Nome: ${users[i].name}</p>
                         <p>E-mail: <u>${users[i].email}</u></p>
                     </div>
                     <div class="card-action">
-                        <button class="btn-small light-blue lighten-2 editUser" onClick="editUser('${i}')" disabled>Editar</button>
+                        <button class="btn-small light-blue editUser" onClick="editUser('${i}')" disabled>Editar</button>
                         <button class="btn-small red darken-2 removeUser" onClick="removeUser('${i}')" disabled>Apagar</button>
                     </div>
                 </div>
@@ -147,7 +160,7 @@ function editUser(index) {
 function editName() {
     if (updateName.value.length < 2 || updateName.value.length > 24) {
         upNameHelper.classList.remove("green")
-        upNameHelper.classList.add("red", "lighten-1")
+        upNameHelper.classList.add("red")
         return upNameHelper.innerHTML = `2 á 24 Caractéres`
     }
     else if (
@@ -159,26 +172,27 @@ function editName() {
     }
     else {
         upNameHelper.classList.remove("green")
-        upNameHelper.classList.add("red", "lighten-1")
+        upNameHelper.classList.add("red")
         return upNameHelper.innerHTML = `Nome invalido!`
     }
 }
 function editEmail() {
     if (updateEmail.value === "") {
         upEmailHelper.classList.remove("green")
-        upEmailHelper.classList.add("red", "lighten-1")
+        upEmailHelper.classList.add("red")
         return upEmailHelper.innerHTML = `Campo obrigatório`
     }
     else if (
         updateEmail.value.match(isEmail)
     ) {
+        users.find(isSigned)
         upEmailHelper.classList.remove("red")
         upEmailHelper.classList.add("green")
         return upEmailHelper.innerHTML = `E-Mail valido!`
     }
     else {
         upEmailHelper.classList.remove("green")
-        upEmailHelper.classList.add("red", "lighten-1")
+        upEmailHelper.classList.add("red")
         return upEmailHelper.innerHTML = `E-Mail invalido!`
     }
 }
